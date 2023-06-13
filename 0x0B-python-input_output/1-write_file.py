d72ff1de25195ba9353a5f4760ea8f467745f30f
DzilafhoPia <dzilafhopia17@gmail.com>
2023-06-13 14:07:02 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,11 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+""""Defines a text file line-counting func."""
 
-"""Defines a file-writing func."""
+def number_of_lines(filename=""):
+    """Returns the numb of lines in a text file."""
+    lines = 0
+    with open(filename) as f:
+        for line in f:
+            lines += 1
 
-def write_file(filename="", text=""):
-    """Write a str to a UTF8 text file.
-    Args:
-        filename (str): The name of the file to write.
-        text (str): The text to write to the file.
-    Returns:
-        The numb of char written.
-    """
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+    return
